79e11ad023cecabd028c6778242ac9d2fe28b31f
Raymond Mariano <rcm@hawaii.edu>
2015-08-29 13:39:38 -1000

    added angular cdnjs

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ var path = require('path');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+var server = app.listen(8080, function() {
+      console.log('I am da servah!');
+    });
