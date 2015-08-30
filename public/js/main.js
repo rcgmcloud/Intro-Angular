90204f46f66ebebf5159356c57f20fb85d510998
Raymond Mariano <rcm@hawaii.edu>
2015-08-29 20:05:00 -1000

    inject and configure

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,25 @@
 (function() {
-  var app = angular.module('myApp', ['ngResource']);
+  var app = angular.module('myApp', ['ngResource', 'ngRoute']);
+
+  app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      // .when('/book', {
+      //   templateUrl: 'views/books.html',
+      //   controller: 'BookController'
+      // })
+      // .when('/movies', {
+      //   templateUrl:'views/movies.html',
+      //   controller: 'MovieController'
+      // })
+      // .when('/other', {
+      //   templateUrl:'views/other.html',
+      //   controller: 'OtherController'
+      // });
+        .when('/', {
+          templateUrl:'views/default.html',
+          controller: 'myController'
+        });
+  }]);
 
   app.run(function ($rootScope, APP_VERSION){
     $rootScope.APP_VERSION = APP_VERSION;
