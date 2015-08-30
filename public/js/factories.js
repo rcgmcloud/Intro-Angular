8e805c3780662ced5731c5e586c7283aab217656
Raymond Mariano <rcm@hawaii.edu>
2015-08-29 15:46:07 -1000

    create an injectable factory

---
+++ public/js/factories.js
@@ -0,0 +1,7 @@
+(function() {
+  angular
+    .module('myApp')
+    .factory('CharacterVersionFactory', function() {
+        return ["Peter", "Meg", "Chris"];
+     });
+})();
--- public/js/myController.controller.js
+++ public/js/myController.controller.js
@@ -1,9 +1,11 @@
 (function(){
   angular.module('myApp')
-    .controller('myController', ['$scope', 'myAppTitle', function ($scope, myAppTitle ){
+    .controller('myController', ['$scope', 'myAppTitle', 'CharacterVersionFactory', function ($scope, myAppTitle , CharacterVersionFactory ){
       // console.log(APP_VERSION);
 
       $scope.myModel = "Cello & Violin";
       $scope.title = myAppTitle;
+      $scope.characters = CharacterVersionFactory;
+
   }]);
 })();
